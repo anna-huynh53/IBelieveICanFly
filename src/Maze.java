81b862407489fc66bcdf58d08cdb2498cfc00da0
Peter Arnott <peterarenot@gmail.com>
2016-05-11 03:24:07 +1000

    a wall is not an item guys

--- src/Maze.java
+++ src/Maze.java
@@ -19,7 +19,7 @@ public class Maze {
 		// initialise all tiles to be wall tiles
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
-				Tile t = new Tile(Tile.WALL, false, false, false);
+				Tile t = new Tile(Tile.WALL, true, false, false);
 				tiles[i][j] = t; 
 			}
 		}	
