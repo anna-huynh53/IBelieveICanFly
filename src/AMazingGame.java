05f7005aa46055490e8c169bfc0b10cc809ee8bf
Benjamin Siriprayook <siriprayook@gmail.com>
2016-05-10 12:39:07 +1000

    just some thoughts and comments on AmazingGame

--- src/AMazingGame.java
+++ src/AMazingGame.java
@@ -1,12 +1,34 @@
 
 public class AMazingGame {
-	
-	public static void main(String args[]) {
-		
+
+	public static main(String args[]) {
+		int gameScore;
+		// take to GUI handler
+		// UserInput i = new UserInput();
+		runGame();		
 	}
 	
 	public void runGame() {
-		
+		// should this be called from amazGame or gui?
+		// should rungame be given an int size from gui?
+		Maze currentGame = new Maze(size);
+		while(!isGameOver(game)) {
+			// arrow key input 
+			// using keylistener and keyEvents?
+
+			/* something along these lines? e.g. 
+			if (e.getKeyCode() == KeyEvent.VK_LEFT)
+        			currentGame.move(left);
+    			else if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+        			currentGame.move(right);
+			else if (e.getKeyCode() == KeyEvent.VK_UP)
+				currentGame.move(up);
+			else if (e.getKeyCode() == KeyEvent.VK_DOWN)
+        			currentGame.move(down);		
+			*/
+		}
+		generateScore(currentGame);
+		// then take to GUI for button press to exit or restart?
 	}
 	
 	public int generateScore(Maze m) {
@@ -14,7 +36,7 @@ public class AMazingGame {
 		
 	}
 	
-	public void clearGame(Maze m) {
-		
+	public void restartGame(Maze m) {
+		m = new Maze(size); // are we deleting or making new maze?
 	}
 }
