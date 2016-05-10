063833d24b1614c2a6699c47689a0a6bdcaf07d8
Peter Arnott <peterarenot@gmail.com>
2016-05-10 13:21:06 +1000

    Added the arraylist

--- src/Player.java
+++ src/Player.java
@@ -1,13 +1,13 @@
-
+import java.util.ArrayList;
 public class Player {
 	private int locX;
 	private int locY;
-	private int[][] pastLocs;
+	private ArrayList<Point> pastLocs;
 	
 	public Player(int locX, int locY) {
 		this.locX = locX;
 		this.locY = locY;
-		pastLocs = new int[][]; //all past x and y or just trail
+		pastLocs = new ArrayList<Point>(); //all past x and y or just trail
 	}
 	
 	public void movePlayer() {
