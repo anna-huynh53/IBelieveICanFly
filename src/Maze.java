b0ed3791ae000ada0eb0b48df047522e107f6111
anna-huynh53 <anna.huynh53@gmail.com>
2016-05-11 12:14:16 +1000

    Update Maze.java

--- src/Maze.java
+++ src/Maze.java
@@ -4,6 +4,10 @@ import java.util.Random;
 public class Maze {
 	private int size;
 	private Tile[][] tiles;
+	private static String START = "start";
+	private static String PATH = "path";
+	private static boolean T = true;
+	private static boolean F = false;
 	//private Player player;
 	
 	/**
@@ -11,66 +15,78 @@ public class Maze {
 	 * @param size The height/width of the square maze that will be created
 	 */
 	public Maze(int size) {
+		tiles = new Tile[size][size];			
 		
-		this.size = size;
-		tiles = new Tile[size][size];
-		//this.player = new Player(size, size);			
-		
-		// initialise all tiles to be wall tiles
+		// initialise all tiles to have all edges i.e. all walls
+		ArrayList<Point> all = new ArrayList<Point>();
+		ArrayList<Point> toVisit = new ArrayList<Point>();
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
-				Tile t = new Tile(Tile.WALL, true, false, false);
-				tiles[i][j] = t; 
+				tiles[i][j] = new Tile(PATH);
+				boolean[] edges = new boolean[4];
+				edges[0] = edges[1] = edges[2] = edges[3] = T;
+				tiles[i][j].setEdges(edges);
+				Point p = new Point(i, j);
+				all.add(p);
+				toVisit.add(p);
 			}
-		}	
+		}
 		
 		// start tile
-		Tile startTile = tiles[0][0];
-		startTile.setClassification(Tile.START);
-		startTile.setTraversable(true);
+		tiles[0][0].setClassification(START);
 		Point startPoint = new Point(0, 0);
+		Point curr = startPoint;
+		toVisit.remove(curr);
 		
 		
-		// tiles that have been visited
-		ArrayList<Tile> visited = new ArrayList<Tile>();
-		visited.add(startTile);
-		
-		// choose a random neighbour tile from the current tile from the toVisit
-		// list and make it a path. Add the visited. Continue until toVisit is empty.
-		Tile curr = startTile;
-		
-		Point currPoint = startPoint;
-		Random rand = new Random();
-		
-		
-		
-		ArrayList<Tile> toVisit = new ArrayList<Tile>();
-		
+		Stack<Point> backStack = new Stack<Point>();
+		Random rand = new Random();	
 		while (!toVisit.isEmpty()) {
-			
-			System.out.println("HERE");
-			
-			// find random neighbour from the curr in toVisit list
-			ArrayList<Tile> neighbours = new ArrayList<Tile>();
-			Point p1 = new Point(currPoint.getX()+1, currPoint.getY());
-			
-			int currX = p1.getX();
-			int currY = p1.getY();
-			Tile t1 = tiles[currX+1][currY]; 
-			if (toVisit.contains(t1)) neighbours.add(t1);
-			Tile t2 = tiles[currX-1][currY];
-			if (toVisit.contains(t2)) neighbours.add(t1);
-			Tile t3 = tiles[currX][currY+1];
-			if (toVisit.contains(t3)) neighbours.add(t1);
-			Tile t4 = tiles[currX][currY];
-			if (toVisit.contains(t4)) neighbours.add(t1);
-			Tile neighbour = neighbours.get(rand.nextInt(neighbours.size()));
-			// set the chosen neighbour to a path
-			neighbour.setClassification(Tile.PATH);
-			neighbour.setTraversable(true);
-			visited.add(neighbour);
-			toVisit.remove(neighbour);
-			curr = neighbours.get(rand.nextInt(neighbours.size()));
+			ArrayList<Point> neighbours = new ArrayList<Point>();
+			Point p1 = new Point(curr.getX(), curr.getY()-1);
+			if (all.contains(p1) && toVisit.contains(p1)) neighbours.add(p1);
+			Point p2 = new Point(curr.getX()+1, curr.getY());
+			if (all.contains(p2) && toVisit.contains(p2)) neighbours.add(p2);
+			Point p3 = new Point(curr.getX(), curr.getY()+1);
+			if (all.contains(p3) && toVisit.contains(p3)) neighbours.add(p3);
+			Point p4 = new Point(curr.getX()-1, curr.getY());
+			if (all.contains(p4) && toVisit.contains(p4)) neighbours.add(p4);
+
+			if (neighbours.size() != 0) {
+				Point neighbour = neighbours.get(rand.nextInt(neighbours.size()));
+				Tile currTile = tiles[curr.getX()][curr.getY()];
+				Tile neighbourTile = tiles[neighbour.getX()][neighbour.getY()];
+				backStack.push(curr);
+				 boolean[] currEdges = currTile.getEdges();
+				 boolean[] neighbourEdges = neighbourTile.getEdges();
+
+				if (neighbour.equals(p1)) {
+					currEdges[0] = F;
+					currTile.setEdges(currEdges);
+					neighbourEdges[2] = F; 
+					neighbourTile.setEdges(neighbourEdges);
+				} else if (neighbour.equals(p2)) {
+					currEdges[1] = F; 
+					currTile.setEdges(currEdges);
+					neighbourEdges[3] = F; 
+					neighbourTile.setEdges(neighbourEdges);	
+				} else if (neighbour.equals(p3)) {
+					currEdges[2] = F; 
+					currTile.setEdges(currEdges);
+					neighbourEdges[0] = F; 
+					neighbourTile.setEdges(neighbourEdges);	
+				} else if (neighbour.equals(p4)) {
+					currEdges[3] = F;
+					currTile.setEdges(currEdges);
+					neighbourEdges[1] = F;
+					neighbourTile.setEdges(neighbourEdges);	
+				}
+
+				curr = neighbour;
+				toVisit.remove(curr);
+			} else {
+				curr = backStack.pop();
+			}
 		}
 	}		
 
