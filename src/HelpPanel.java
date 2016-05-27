b294e10cd08d9c6fedb2dde751e521b0c3d620d2
siriprayook7 <siriprayook@gmail.com>
2016-05-27 13:11:27 +1000

    new font and colour

--- src/HelpPanel.java
+++ src/HelpPanel.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
+import java.awt.Color;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -22,11 +23,24 @@ public class HelpPanel extends JPanel{
 	public HelpPanel() throws IOException{
 		this.setPreferredSize(new Dimension(850, 950));
 		this.setLayout(new FlowLayout());
-		this.title = new JLabel("How to play the game");
+		this.title = new JLabel("How to play");
 		this.helpText = new JTextArea();
 		this.play = new JButton("Play");
 		this.back = new JButton("Back");
+		this.setBackground(new Color(27, 192, 247));
+
+		//font
+		try {
+			Font customFont = Font.createFont(Font.TRUETYPE_FONT, new FileInputStream(new File("res/sunshineFont2.ttf"))).deriveFont(Font.PLAIN, 22);
+		    	helpText.setFont(customFont);
+		    	Font customTitle = Font.createFont(Font.TRUETYPE_FONT, new FileInputStream(new File("res/sunshineFont.ttf"))).deriveFont(Font.PLAIN, 32	);
+		    	title.setFont(customTitle);
+		} catch (FontFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
 		
+		// textArea options
 	    helpText.setLineWrap(true);
 	    helpText.setWrapStyleWord(true);
 	    helpText.setOpaque(false);
@@ -44,8 +58,8 @@ public class HelpPanel extends JPanel{
 	    helpText.setText(text);
 		
 	    // sets fonts
-	    title.setFont(new Font("Impact",1,40));
-	    helpText.setFont(new Font("Calibri",1,18));
+	    // title.setFont(new Font("Impact",1,40));
+	    // helpText.setFont(new Font("Calibri",1,18));
 	    play.setFont(new Font("Impact",1,18));
 	    back.setFont(new Font("Impact",1,18));
 	    
